09fde496f483bf3dc826836049d27204954eb581
hsny91 <husniyesakmak.91@gmail.com>
2021-11-10 15:52:33 +0100

    node file was cretaed

--- mongodbExercise/mongodb/expressMongoDb.js
+++ mongodbExercise/mongodb/expressMongoDb.js
@@ -2,11 +2,12 @@ var mongoose = require("mongoose");
 var Customer = require("./schema/customer");
 
 mongoose.connect(
-  "mongodb+srv://husniye:sakmak4326@cluster0.s7lvi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  "mongodb+srv://husniye:sakmak4326@cluster0.nviij.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   (error) => {
     if (!error) {
       console.log("connected to mongo db");
     }
+    console.log(error)
   }
 );
 
@@ -37,9 +38,9 @@ mongoose.connect(
 //   console.log(data);
 // });
 
-Customer.find({ }, (error, data) => {
-  if (error) {
-    throw error;
-  }
-  console.log(data);
-}).where('city').equals('istanbul').sort('-name').limit(2).select('name')
+// Customer.find({ }, (error, data) => {
+//   if (error) {
+//     throw error;
+//   }
+//   console.log(data);
+// }).where('city').equals('istanbul').sort('-name').limit(2).select('name')
